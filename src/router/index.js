e708133410c64a0a94355e545fc1a65104d6f71f
Jaycee <jcdbd214085@gmail.com>
2025-05-09 13:44:56 +0800

    新增About Faq Guide的Vue page及新增至路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,99 +1,121 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import { useLoadingStore } from '@/stores/loadingStore'
+import {createRouter, createWebHistory} from 'vue-router'
+import {useLoadingStore} from '@/stores/loadingStore'
 import AlertDemo from '@/views/alert/AlertDemo.vue'
 import InputDemo from '@/views/input/InputDemo.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: () => import('@/views/HomeView.vue')
-    },
-    //聊天室
-    {
-      path: '/chat',
-      name: 'chat',
-      component: () => import('@/views/chat/ChatView.vue')
-    },
-    {
-      path: '/alert-demo',
-      name: 'AlertDemo',
-      component: AlertDemo
-    },
-    {
-      path: '/input-demo',
-      name: 'InputDemo',
-      component: InputDemo
-    },
-    {
-      path: '/landlord',
-      name: 'Landlord',
-      component: () => import('@/views/landlord/landlord.vue')
-    },
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: () => import('@/views/HomeView.vue')
+        },
+        //聊天室
+        {
+            path: '/chat',
+            name: 'chat',
+            component: () => import('@/views/chat/ChatView.vue')
+        },
+        {
+            path: '/alert-demo',
+            name: 'AlertDemo',
+            component: AlertDemo
+        },
+        {
+            path: '/input-demo',
+            name: 'InputDemo',
+            component: InputDemo
+        },
+        {
+            path: '/landlord',
+            name: 'Landlord',
+            component: () => import('@/views/landlord/landlord.vue')
+        },
 
-    //註冊畫面 測試用
-    {
-      path: '/login',
-      name: 'login',
-      component: () => import('@/components/login/LoginModal.vue')
-    },
+        //註冊畫面 測試用
+        {
+            path: '/login',
+            name: 'login',
+            component: () => import('@/components/login/LoginModal.vue')
+        },
 
-    //側邊欄 測試用
-    {
-      path: '/sidemenu',
-      name: 'sidemenu',
-      component: () => import('@/components/SideMenu.vue')
-    },
+        //側邊欄 測試用
+        {
+            path: '/sidemenu',
+            name: 'sidemenu',
+            component: () => import('@/components/SideMenu.vue')
+        },
 
-    //彈窗 測試用
-    {
-      path: '/inputmodel',
-      name: 'inputmodel',
-      component: () => import('@/components/InputModal.vue')
-    },
-    //個人頁面
-    {
-      path: '/userhome',
-      name: 'userhome',
-      component: () => import('@/views/UserHome/UserHome.vue')
-    },
-
-    //分頁元件 測試用
-    {
-      path: '/pagination',
-      name: 'pagination',
-      component: () => import('@/components/Pagination/Pagination.vue')
-    },
-
-    //房屋搜尋
-    {
-      path: '/PropertySearch',
-      name: 'property-search-view',
-      component: () => import('@/views/PropertySearch/PropertySearchView.vue')
-    },
+        //彈窗 測試用
+        {
+            path: '/inputmodel',
+            name: 'inputmodel',
+            component: () => import('@/components/InputModal.vue')
+        },
         //個人頁面
         {
-          path: '/UserHome/UserProfile',
-          name: 'UserProfile',
-          component: () => import('@/views/UserHome/UserProfile.vue')
-        }
+            path: '/userhome',
+            name: 'userhome',
+            component: () => import('@/views/UserHome/UserHome.vue')
+        },
 
-  ],
+        //分頁元件 測試用
+        {
+            path: '/pagination',
+            name: 'pagination',
+            component: () => import('@/components/Pagination/Pagination.vue')
+        },
+
+        //房屋搜尋
+        {
+            path: '/PropertySearch',
+            name: 'property-search-view',
+            component: () => import('@/views/PropertySearch/PropertySearchView.vue')
+        },
+        //個人頁面
+        {
+            path: '/UserHome/UserProfile',
+            name: 'UserProfile',
+            component: () => import('@/views/UserHome/UserProfile.vue')
+        },
+        //公告
+        {
+            path: '/notice',
+            name: 'notice',
+            component: () => import('@/views/notice/Notice.vue')
+        },
+        {
+            path: '/guide',
+            name: 'guide',
+            component: () => import('@/views/guide/Guide.vue')
+        },
+        {
+            path: '/faq',
+            name: 'faq',
+            component: () => import('@/views/faq/Faq.vue')
+        },
+        {
+            path: '/about',
+            name: 'about',
+            component: () => import('@/views/about/About.vue')
+        },
+
+
+    ],
 })
 
 router.beforeEach((to, from, next) => {
-  const loadingStore = useLoadingStore()
-  loadingStore.show()
-  next()
+    const loadingStore = useLoadingStore()
+    loadingStore.show()
+    next()
 })
 
 router.afterEach(() => {
-  const loadingStore = useLoadingStore()
-  setTimeout(() => {
-    loadingStore.hide()
-  }, 300)
+    const loadingStore = useLoadingStore()
+    setTimeout(() => {
+        loadingStore.hide()
+    }, 300)
 })
 
 export default router
