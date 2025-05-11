d4d02b6129e4a206b33ae1448a9ac1473af05ec0
Rain Lin <xwm1593575@gmail.com>
2025-05-11 16:52:37 +0800

    彈窗響應式調整 待加入網頁再解開測試

--- src/components/login/LoginModal.vue
+++ src/components/login/LoginModal.vue
@@ -364,6 +364,21 @@ body {
   overflow: hidden;
 }
 
+/* 彈窗響應式調整 */
+/* 原本固定寬度 850px 改為百分比 + 最大寬度限制 */
+/* .container {
+  position: relative;
+  width: 90%;
+  max-width: 850px;
+  height: 550px;
+  background: #f0f9f8;
+  margin: 20px auto; 
+  border-radius: 30px;
+  box-shadow: 0 0 30px rgba(0, 0, 0, 0.2);
+  overflow: hidden;
+} */
+
+
 /*  表單內 h1 標題大小 */
 .container h1 {
   font-size: 36px;
