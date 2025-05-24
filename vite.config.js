11f7e98a94735146ef29bb725532a5c597ca0023
Jaycee <jcdbd214085@gmail.com>
2025-05-24 16:58:56 +0800

    正式部屬版，修改了env全域變數，因為沒有ssl證書所以改為http 而非https 目標端口改為80 因為後端asp端口是http 端口80

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { fileURLToPath, URL } from "node:url";
-import { defineConfig, loadEnv } from "vite";
-import vue from "@vitejs/plugin-vue";
-import vueDevTools from "vite-plugin-vue-devtools";
+import { defineConfig, loadEnv } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
+import { fileURLToPath } from 'url'
 
 export default defineConfig(({ mode }) => {
   // ⬇️ 載入 .env 變數
@@ -15,6 +15,9 @@ export default defineConfig(({ mode }) => {
       },
     },
     server: {
+      host: "0.0.0.0",
+      port: 3000,
+      allowedHosts: ["vue.jayceeswlrorobot.win", "localhost"],
       proxy: {
         "/api": {
           target: env.VITE_API_BASE_URL,
@@ -29,5 +32,5 @@ export default defineConfig(({ mode }) => {
         },
       },
     },
-  };
-});
+  }
+})
